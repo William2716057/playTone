65dbfda81d038b1a646d0bdc41394a69248bd09d
William <willspri@gmail.com>
2024-07-28 08:21:23 +1200

    committed

---
+++ playTone.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pyaudio
+
+def generate_tone(frequency, duration, sample_rate, amplitude=0.5):
+    #generate sine wave
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+    signal = amplitude * np.sin(2 * np.pi * frequency * t)
+    return signal
+
+def play_sound(signal, sample_rate):
+    #play the audio
+    p = pyaudio.PyAudio()
+    
+    #open stream
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=sample_rate,
+                    output=True)
+    
+    #convert signal to bytes and play
+    stream.write(signal.astype(np.float32).tobytes())
+    
+    #close stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+def main():
+    #parameters
+    sample_rate = 44100  #sample rate
+    ultrasound_freq = 440  #frequency in Hz (22000)
+    duration = 15 #Duration in seconds
+    
+    #generate tone
+    ultrasound_signal = generate_tone(ultrasound_freq, duration, sample_rate)
+    
+    #play tone
+    play_sound(ultrasound_signal, sample_rate)
+    
+    print("playing tone... ")
+
+if __name__ == "__main__":
+    main()
